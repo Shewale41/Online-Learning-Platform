add72053731ff7f738d95648c85f52b26da0dd5e
Shewale41 <shewalegopal4119@gmail.com>
2025-08-01 21:29:52 +0530

    my first commit

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,14 @@
+
+import { type Metadata } from 'next'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -18,12 +29,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
+    
   );
 }
