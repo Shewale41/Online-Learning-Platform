5d6e1ae4f1d5a5375e17a709a62b49ed4e095151
Shewale41 <shewalegopal4119@gmail.com>
2025-08-01 22:24:47 +0530

    signin2

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 //this is for all manjhe sagle user sathi he yenar ahe
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
 
+//this is protecting all the routes
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect()
